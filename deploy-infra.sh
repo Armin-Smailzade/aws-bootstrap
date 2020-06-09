18d880ff5be8e0e86cada1a0883bb1bcf1efb63c
EC2 Default User <ec2-user@ip-172-31-52-60.ec2.internal>
2020-06-09 17:36:15 +0000

    changed owner

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -18,16 +18,16 @@ GH_REPO=$(cat ~/.github/aws-bootstrap-repo)
 GH_BRANCH=master
 
 # Deploys static resources
-echo -e "\n\n=========== Deploying setup.yml ==========="
-aws cloudformation deploy \
-  --region $REGION \
-  --profile $CLI_PROFILE \
-  --stack-name $STACK_NAME-setup \
-  --template-file setup.yml \
-  --no-fail-on-empty-changeset \
-  --capabilities CAPABILITY_NAMED_IAM \
-  --parameter-overrides \
-    CodePipelineBucket=$CODEPIPELINE_BUCKET
+#echo -e "\n\n=========== Deploying setup.yml ==========="
+#aws cloudformation deploy \
+#  --region $REGION \
+#  --profile $CLI_PROFILE \
+#  --stack-name $STACK_NAME-setup \
+#  --template-file setup.yml \
+#  --no-fail-on-empty-changeset \
+#  --capabilities CAPABILITY_NAMED_IAM \
+#  --parameter-overrides \
+#    CodePipelineBucket=$CODEPIPELINE_BUCKET
 
 # Deploy the CloudFormation template
 echo -e "\n\n=========== Deploying main.yml ==========="
